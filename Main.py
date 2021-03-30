419fe8a756dde3a531c043752aec1358461d3c42
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-03-30 21:33:56 +0100

    Tiny changes

--- Main.py
+++ Main.py
@@ -4,12 +4,11 @@ import requests
 warnings.filterwarnings("ignore", message="Could not import the lzma module.")
 
 
-import Explore as e
+import link_gatherer as e
 import BBC_text as bb
 
 bbc_news = e.Newscraper('http://bbc.co.uk','a',
         'ssrcss-10ivm7i-PromoLink e1f5wbog5')
 
-x,y = bbc_news.dataframe_collection()
+x = bbc_news.dataframe_collection()
 j = bb.text_extraction(x)
-print(j.head(10))
