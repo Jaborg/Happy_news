06ae0071a8488e54b4c21464dd1b0b2dd0ba2654
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-04-16 17:33:56 +0100

    Changed test file to Polarisation. Added functionality to main file, yet to test it.

--- Main.py
+++ Main.py
@@ -9,18 +9,15 @@ warnings.filterwarnings("ignore", message="Could not import the lzma module.")
 
 import link_gatherer as e
 import BBC_text as bb
+import Polarity as pol
 
 bbc_news = e.Newscraper('http://bbc.co.uk','a',
         'ssrcss-10ivm7i-PromoLink e1f5wbog5')
 
-x = bbc_news.dataframe_collection()
-j = bb.text_extraction(x)
+main_links = bbc_news.dataframe_collection()
+extracted_texts = bb.text_extraction(main_links)
+polarised_text = pol.polarity_table(main_links['Id'].to_list())
 
 
-
-x.apply(lambda row : db.insert_sql('links',('Id,Date,Title,Link'),(row.Id,row.Date,row.Title,row.Link)), axis = 1)
-j.apply(lambda row : db.insert_sql('texts',('Id,Text'),(row.Id,row.Text)),axis=1)
-# x.to_sql('links',if_exists = 'append',con = con,index=False, index_label='Id')
-# j.to_sql('texts',if_exists = 'append',con = con,index=False, index_label='Id')
-
-# use the generic Exception, both IntegrityError and sqlite3.IntegrityError caused trouble.
+main_links.apply(lambda row : db.insert_sql('links',('Id,Date,Title,Link'),(row.Id,row.Date,row.Title,row.Link)), axis = 1)
+extracted_texts.apply(lambda row : db.insert_sql('texts',('Id,Text'),(row.Id,row.Text)),axis=1)
---
+++ Polarity.py
@@ -0,0 +1,27 @@
+import warnings
+import requests
+import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
+import pandas as pd
+
+nlp = spacy.load("en_core_web_sm")
+nlp.add_pipe('spacytextblob')
+warnings.filterwarnings("ignore")
+
+from db_utils import db_connect
+
+con = db_connect()  # connect to the database
+cur = con.cursor() # instantiate a cursor obj
+
+
+def polarity_table(ids : list) -> pd.DataFrame:
+    sql = 'select id,text from texts where id in ({ids});'.format(ids=ids)
+
+    text = cur.execute(sql).fetchall()
+    polarity = pd.DataFrame(columns=['Id','Polarity','Length'])
+    for lexi in text:
+         doc = nlp(lexi[1])
+         length,pol,id = len([token.text for token in doc]),doc._.polarity,lexi[0]
+         polarity_sub = pd.DataFrame(data=[(id,pol,length)],columns=['Id','Polarity','Length'])
+         polarity = polarity.append(polarity_sub)
+    return polarity
