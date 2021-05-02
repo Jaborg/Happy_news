3cdd479a469e32dea7b1652711bc9872cb239216
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-05-02 16:31:58 +0100

    Adding in Guardian data set

--- Main.py
+++ Main.py
@@ -11,14 +11,21 @@ import link_gatherer as e
 import BBC_text as bb
 import Polarity as pol
 
-bbc_news = e.Newscraper('http://bbc.co.uk','a',
-        'ssrcss-10ivm7i-PromoLink e1f5wbog5')
+# bbc_news = e.Newscraper('http://bbc.co.uk','a',
+#         'ssrcss-10ivm7i-PromoLink e1f5wbog5')
 
-main_links = bbc_news.dataframe_collection()
-main_links.apply(lambda row : db.insert_sql('links',('Id,Date,Title,Link'),(row.Id,row.Date,row.Title,row.Link)), axis = 1)
+guard_news = e.Newscraper('https://www.theguardian.com/uk','a',
+        'u-faux-block-link__overlay js-headline-text')
 
-extracted_texts = bb.text_extraction(main_links)
-extracted_texts.apply(lambda row : db.insert_sql('texts',('Id,Text'),(row.Id,row.Text)),axis=1)
+main_links = guard_news.dataframe_collection()
+print(main_links.Link)
 
-polarised_text = pol.polarity_table(main_links['Id'].to_list())
-polarised_text.apply(lambda row : db.insert_sql('polarity',('Id,Polarity,Length'),(row.Id,row.Polarity,row.Length)), axis = 1)
+
+# main_links = bbc_news.dataframe_collection()
+# main_links.apply(lambda row : db.insert_sql('links',('Id,Date,Title,Link'),(row.Id,row.Date,row.Title,row.Link)), axis = 1)
+#
+# extracted_texts = bb.text_extraction(main_links)
+# extracted_texts.apply(lambda row : db.insert_sql('texts',('Id,Text'),(row.Id,row.Text)),axis=1)
+#
+# polarised_text = pol.polarity_table(main_links['Id'].to_list())
+# polarised_text.apply(lambda row : db.insert_sql('polarity',('Id,Polarity,Length'),(row.Id,row.Polarity,row.Length)), axis = 1)
---
+++ webconnect.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from db_utils import db_connect
+
+con = db_connect()  # connect to the database
+cur = con.cursor() # instantiate a cursor obj
+
+
+
+
+db = 'SELECT * FROM LINKS;'
+
+db = cur.execute(db).fetchall()
+#
+# @app.get("/")
+# async def root():
+#     return {"message": "Abdullah is happy"}
+#
+#
+@app.get("/positive_new")
+def get_news():
+    return db
+#
+# @app.post("/articles")
+# def post_news(db: list):
+#     return db
