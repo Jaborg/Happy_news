2896342fc9d729c4fff2b5eabafabd51b34f0a5d
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-05-10 20:06:11 +0100

    Testing FastAPI

--- app/backend/webconnect.py
+++ app/backend/webconnect.py
@@ -2,26 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from db_utils import db_connect
-
-con = db_connect()  # connect to the database
-cur = con.cursor() # instantiate a cursor obj
 
 
 
 
-db = 'SELECT * FROM LINKS;'
 
-db = cur.execute(db).fetchall()
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
+
 #
-# @app.get("/")
-# async def root():
-#     return {"message": "Abdullah is happy"}
-#
-#
-@app.get("/positive_new")
-def get_news():
-    return db
+# @app.get("/positive_new")
+# def get_news():
+#     return 'yo'
 #
 # @app.post("/articles")
 # def post_news(db: list):
