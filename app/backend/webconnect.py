2ac2e619a66f6f56df2cf72b8a1bd0a26bb5ab95
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-05-11 14:23:58 +0100

    Creating connection to database in FastAPI file

--- app/backend/webconnect.py
+++ app/backend/webconnect.py
@@ -3,7 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+import sqlite3
 
+con = sqlite3.connect(r'/Users/jacob/workspace/data_eng/database.sqlite3')
+cur = con.cursor() # instantiate a cursor obj
+
+sql = '''select * from links'''
+x = cur.execute(sql).fetchall()
+print(x)
 
 
 
--- db_utils.py
+++ db_utils.py
@@ -5,7 +5,7 @@ import os
 
 
 DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
-
+print(DEFAULT_PATH)
 def db_connect(db_path=DEFAULT_PATH ) ->  sqlite3.Connection:
     con = sqlite3.connect(db_path)
     return con
