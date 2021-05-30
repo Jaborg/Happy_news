88b2f149b2c2a2ce5cad61df8f8352fb3f4065cf
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-05-30 11:17:50 +0100

    Trying to add DailyMail to scrape

--- app/scraping/link_opener.py
+++ app/scraping/link_opener.py
@@ -6,3 +6,12 @@ def open_link(link: str,letter: str ,class_:str) -> list:
     soup = BeautifulSoup(r.content,"html.parser")
     links = soup.find_all(letter, class_=class_)
     return links
+
+
+d = open_link('https://www.dailymail.co.uk/news/index.html','h2','linkro-darkred')
+
+no = 0
+for e in d:
+    print(e.a.text,no )
+    print('\n')
+    no += 1
