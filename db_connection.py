9be27bad19c9175ceb757923068228b1b97c02c9
Jacob Lappin Sadiq <jlappinsadiq@gmail.com>
2021-06-08 15:03:05 +0100

    Resolved new data extraction method

--- db_connection.py
+++ db_connection.py
@@ -10,7 +10,6 @@ def insert_sql(table : str ,columns : tuple ,values: tuple) -> None:
     cur = con.cursor() # instantiate a cursor obj
     sql = '''INSERT or IGNORE into {table}({columns}) VALUES {values};
                             '''.format(table=table,columns=columns,values=values)
-    print(sql)
     cur.execute(sql)
     con.commit()
 
